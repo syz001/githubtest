70bcdf2d1957b58de696ffe17c81eaa4cc8962e8
sy001 <hero_sang@126.com>
2020-09-28 17:19:06 +0800

    项目发布

---
+++ src/com/chinasoifti/demo/Demo.java
@@ -0,0 +1,8 @@
+package com.chinasoifti.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        //https://github.com/syz001/githubtest.git
+        System.out.println(111);
+    }
+}
