b8d4b577eb7d5e508a9e0f631fc399335b3c4b32
sy001 <hero_sang@126.com>
2020-09-28 19:03:08 +0800

    地产一次更想333

--- src/com/chinasoifti/demo/Demo.java
+++ src/com/chinasoifti/demo/Demo.java
@@ -3,7 +3,7 @@ package com.chinasoifti.demo;
 public class Demo {
     public static void main(String[] args) {
         //https://github.com/syz001/githubtest.git
-        System.out.println(111);
+        System.out.println(111333);
         //你好啊
     }
 }
